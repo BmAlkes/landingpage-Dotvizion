36774b7ba45e228d8f39793317527bbe57f1a4ef
bmalkes <bmalkes@gmail.com>
2025-03-09 20:29:34 +0200

    feat:add connection form with crm

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { ToastContainer } from 'react-toastify';
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer/>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
